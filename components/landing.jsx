8b4d72ea2ff9f8af1fc0bd7cf047c163c6e4421a
Souvik <souvikde.ns@gmail.com>
2021-05-29 01:21:06 +0530

    Update landing.jsx

--- components/landing.jsx
+++ components/landing.jsx
@@ -5,8 +5,8 @@ export default () => {
         <div className="flex">
             <div className="relative lg:ml-36 lg:mt-56 md:ml-24 md:mt-36">
                 <div className="">
-                    <h1 className="md:text-4xl lg:text-6xl font-black text-gray-800">I'm Souvik</h1>
-                    <h2 className="md:text-xl lg:text-2xl font-bold text-gray-600 text-right">Nice to meet you!</h2>
+                    <h1 className="text-6xl font-black text-gray-800">I'm Souvik</h1>
+                    <h2 className="text-2xl font-bold text-gray-600 text-right">Nice to meet you!</h2>
                 </div>
             </div>
             <div className="flex-1" />
