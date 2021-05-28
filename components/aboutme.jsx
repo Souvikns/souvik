43f2679852596aaefc6a5eae3698b90a2c9b7e13
Souvik <souvikde.ns@gmail.com>
2021-05-29 02:10:33 +0530

    Update aboutme.jsx

--- components/aboutme.jsx
+++ components/aboutme.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 
 export default () => {
-    return <div className="w-full">
+    return <div className="">
         <div className="flex justify-center">
+            <div className="flex-1" />
             <div className="p-4">
                 <img className="object-contain w-full" src="/aboutme.svg" alt="aboutme" />
             </div>
+            <div className="flex-1" />
         </div>
     </div>
 }
