c3540d033138b67c1ff688302e03c894016b254e
Souvik <souvikde.ns@gmail.com>
2021-05-29 02:58:29 +0530

    designing aboutme sections ✏

--- components/aboutme.jsx
+++ components/aboutme.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 
 export default () => {
     return <div className="">
-        <div className="flex justify-center">
-            <div className="flex-1" />
-            <div className="p-4">
-                <img src="/aboutme.svg" alt="aboutme" />
-            </div>
-            <div className="flex-1" />
+        <div>
+            <center>
+                <img src="/aboutme.svg" alt="aboutme" width="40%" />
+            </center>
+        </div>
+
+        <div className="mt-10" />
+
+        <div className="mx-32 py-4 px-6 my-4 shadow">
+            <ul className="list-disc list-inside text-xl font-mono text-gray-600">
+                <li>I am undergrad software developer pursing my B-Tech degree from <a className="bold text-red-400" target="_blank" href="https://www.cuchd.in/">Chandigarh University</a>.</li>
+                <li>I love open source and spend most of my day coding.</li>
+                <li>I founded <a className="bold text-red-400" target="_blank" href="https://github.com/tech-Phantoms/">TechPhantoms</a>, a open source community for students.</li>
+                <li>My favorite beverage is ☕</li>
+            </ul>
         </div>
     </div>
 }
