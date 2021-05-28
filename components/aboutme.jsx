cbc488d79714cd7b6851c76332259d8b30136caa
Souvik <souvikde.ns@gmail.com>
2021-05-29 02:03:40 +0530

    designing about me section 🎉

---
+++ components/aboutme.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default () => {
+    return <div className="w-full">
+        <div className="flex justify-center">
+            <div className="p-4">
+                <img className="object-contain w-full" src="/aboutme.svg" alt="aboutme" />
+            </div>
+        </div>
+    </div>
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import Landing from '../components/landing';
+import Aboutme from '../components/aboutme';
+import HSpacing from '../components/hspacing';
 
 export default () => {
     return <div className="sm:px-20">
         <Navbar />
         <Landing />
+        <HSpacing />
+        <Aboutme />
     </div>
 }
