9fded8f8db0c3ceafd528616bb532e1010c81c91
Souvik <souvikde.ns@gmail.com>
2021-05-29 14:14:57 +0530

    Update aboutme.jsx

--- components/aboutme.jsx
+++ components/aboutme.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HSpacing from './hspacing';
 
 export default () => {
-    return <div className="">
+    return <div className="py-2 shadow">
         <div>
             <center>
                 <img src="/aboutme.svg" alt="aboutme" width="40%" />
@@ -11,13 +11,18 @@ export default () => {
 
         <HSpacing />
 
-        <div className="mx-32 py-4 px-6 my-4 shadow">
-            <ul className="list-disc list-inside text-xl font-mono text-gray-600">
-                <li>I am undergrad software developer pursing my B-Tech degree from <a className="bold text-red-400" target="_blank" href="https://www.cuchd.in/">Chandigarh University</a>.</li>
-                <li>I love open source and spend most of my day coding.</li>
-                <li>I founded <a className="bold text-red-400" target="_blank" href="https://github.com/tech-Phantoms/">TechPhantoms</a>, a open source community for students.</li>
-                <li>My favorite beverage is ☕</li>
-            </ul>
+        <div className="flex">
+            <div className="flex-1"/>
+            <div className="text-xl font-mono text-gray-600">
+                <p>
+                    I am undergrad software developer pursing my B-Tech degree from <a className="bold text-red-400" target="_blank" href="https://www.cuchd.in/">Chandigarh University</a>.
+                </p>
+                <p>I love open source and spend my days coding.</p>
+                <p>I co founded <a className="bold text-red-400" target="_blank" href="https://github.com/tech-Phantoms/">TechPhantoms</a>, an open source community for college students.</p>
+                <p>My favorite beverage is ☕</p>
+            </div>
+            <div className="flex-1" />
         </div>
+
     </div>
 }
