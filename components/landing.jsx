3f6ddd0bb4667b51ac3b52132dcd4d556be82136
Restyled.io <commits@restyled.io>
2021-05-31 10:55:04 +0000

    Restyled by prettier

--- components/landing.jsx
+++ components/landing.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 export default () => {
-
-    return <div className="">
-        <div className="flex flex-col-reverse md:flex-row">
-            <div className="self-center">
-                <div className="">
-                    <h1 className="text-6xl font-black text-gray-800">I'm Souvik</h1>
-                    <h2 className="text-2xl font-bold text-gray-600 text-right">Nice to meet you!</h2>
-                </div>
-            </div>
-            <div className="flex-1" />
-
-            <div className="">
-                <img src="/landing-logo.svg" alt="landing-logo" width="400" />
-            </div>
+  return (
+    <div className="">
+      <div className="flex flex-col-reverse md:flex-row">
+        <div className="self-center">
+          <div className="">
+            <h1 className="text-6xl font-black text-gray-800">I'm Souvik</h1>
+            <h2 className="text-2xl font-bold text-gray-600 text-right">
+              Nice to meet you!
+            </h2>
+          </div>
         </div>
+        <div className="flex-1" />
+
+        <div className="">
+          <img src="/landing-logo.svg" alt="landing-logo" width="400" />
+        </div>
+      </div>
     </div>
-}
+  );
+};
