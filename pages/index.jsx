d7b9925aa71ff4f09705921ea54de1acd132d094
Souvik <souvikde.ns@gmail.com>
2021-06-14 10:50:45 +0530

    connecting graphql

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import Navbar from "../components/navbar";
 import Screens from '../components/screens';
 import { SCREEN_PADDING } from '../styleconstants';
+import client from '../apollo-client';
+import { gql } from '@apollo/client';
 
-export default () => {
+export default (props) => {
+  console.log(props.data.user.publication.posts);
   return (
     <div>
       <Navbar />
@@ -14,3 +17,36 @@ export default () => {
     </div>
   );
 };
+
+export async function getStaticProps(context) {
+  const { data } = await client.query({
+    query: gql`
+    query {
+  user(username: "Souvikns"){
+    username
+    name
+    _id
+    tagline
+    numFollowing
+    photo
+    publication {
+      _id
+      author
+      title
+      posts {
+        title
+        slug
+        cuid
+        totalReactions
+        coverImage
+      }
+    }
+  }
+}
+    `
+  })
+  let posts = data.user.publication.posts;
+  return {
+    props: { data, posts }
+  }
+}
