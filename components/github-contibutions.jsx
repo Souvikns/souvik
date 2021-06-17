7b0aed8b81894cd957fdc49c2ed6ef378f7e5e8c
Souvik <souvikde.ns@gmail.com>
2021-06-17 20:53:09 +0530

    Update github-contibutions.jsx

--- components/github-contibutions.jsx
+++ components/github-contibutions.jsx
@@ -1,7 +1,7 @@
-
+import {GRADIENT_TITLE} from '../styleconstants'
 export default () => {
 	return <div className="w-full py-6 dark:text-gray-200">
-		<p className="text-center text-2xl py-4 dark:text-gray-200 text-gray-700 font-black font-mono ">
+		<p className={`text-center text-2xl py-4 font-black font-mono ${GRADIENT_TITLE} `}>
 			Contributions
 		</p>
 		<img className="w-full dark:text-gray-200" src="https://ghchart.rshah.org/Souvikns" alt="github Chart" />
