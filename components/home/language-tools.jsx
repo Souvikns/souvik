24655c09bb5dd52bec5a4ec9e199797e9594ea55
Souvik <souvikde.ns@gmail.com>
2021-10-16 20:04:05 +0530

    feat: designed index page

---
+++ components/home/language-tools.jsx
@@ -0,0 +1,39 @@
+export default () => {
+  return (
+    <div>
+      <h1 className="font-bold">Languages and Tools:</h1>
+      <div className="flex gap-2 pt-2">
+        <code>
+          <img
+            width="30"
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
+          />
+        </code>
+        <code>
+          <img
+            width="30"
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"
+          />
+        </code>
+        <code>
+          <img
+            width="30"
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"
+          />
+        </code>
+        <code>
+          <img
+            width="30"
+            src="https://raw.githubusercontent.com/github/explore/5c058a388828bb5fde0bcafd4bc867b5bb3f26f3/topics/graphql/graphql.png"
+          />
+        </code>
+        <code>
+          <img
+            width="30"
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"
+          />
+        </code>
+      </div>
+    </div>
+  );
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,33 @@
-import Intro from "../components/intro";
-import Navbar from "../components/navbar";
+import Intro from '../components/home/intro';
+import Navbar from "../components/home/navbar";
+import LanguagesTools from '../components/home/language-tools';
+import Blogs from '../components/home/blogs';
+import Stats from '../components/home/stats';
 
 export default () => {
   return (
     <div>
       <Navbar />
 
-      <div className="py-10">
+      <div className="pt-10">
         <Intro />
       </div>
+
+      <div className="pt-5">
+        <LanguagesTools />
+      </div>
+
+      <div className="pt-10">
+        <Blogs />
+      </div>
+
+      <div className="pt-5">
+        <Stats />
+      </div>
+
+      <section id="footer" className="py-20">
+
+      </section>
     </div>
   );
 };
