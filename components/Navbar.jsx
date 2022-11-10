c7aa61ebb8ac0fefeec935ae7465fc39c62f4b01
souvik <souvikde.ns@gmail.com>
2022-11-10 09:10:54 +0530

    feat: create navlinks

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,25 @@
+import Link from "next/link";
+
+const NavLinks = [
+  {title: 'Blogs', href: '/blog'},
+  {title: 'Projects', href: '/project'},
+  {title: 'About', href: '/about'}
+]
 
 const Navbar = () => {
   return (
     <div className="flex flex-col justify-between py-4">
       <header className="flex items-center justify-between">
         <div>
-          <h1 className="font-bold text-3xl font-mono">Souvik</h1>
+          <h1 className="font-extrabold text-3xl font-mono">
+            <Link href={"/"}>Souvik</Link>
+          </h1>
         </div>
         <div className="flex items-center text-base leading-5">
-          {["blogs", "projects", "about"].map((el) => (
-            <h1 className="p-1 font-semibold block">{el}</h1>
+          {NavLinks.map((el) => (
+            <h1 className="p-1 font-semibold sm:p-4">
+              <Link href={el.href}>{el.title}</Link>
+            </h1>
           ))}
         </div>
       </header>
