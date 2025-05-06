ad1b0f1fd577248b41c6b3bf50ab3241f3f17189
Souvik De <souvikde.ns@gmail.com>
2025-05-07 00:41:53 +0530

    feat: add vercel analytics

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -6,6 +6,7 @@ import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import {Analytics} from '@vercel/analytics/next'
 
 const LayoutWrapper = ({ children }) => {
   return (
@@ -46,6 +47,7 @@ const LayoutWrapper = ({ children }) => {
         </header>
         <main className="mb-auto">{children}</main>
         <Footer />
+        <Analytics />
       </div>
     </SectionContainer>
   )
