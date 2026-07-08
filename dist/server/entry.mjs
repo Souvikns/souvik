ffe6f67a7a8db70d17c99cd0ce86dd2e53ed8285
souvik <souvikde.tech@gmail.com>
2026-07-08 09:15:08 +0530

    removing dist

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,7 +1,7 @@
 globalThis.process ??= {};
 globalThis.process.env ??= {};
 import "cloudflare:workers";
-import { w } from "./chunks/worker-entry_BvGrT6om.mjs";
+import { w } from "./chunks/worker-entry_B01rRA-o.mjs";
 export {
   w as default
 };
